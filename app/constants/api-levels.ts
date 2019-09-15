441a5afac9edc94e2c1d2314b677b3ac0b69725c
Justin Qualley <justin@pal-nat186-157-207.itap.purdue.edu>
2019-09-15 01:05:42 -0400

    added text to intro and 1.1

--- app/constants/api-levels.ts
+++ app/constants/api-levels.ts
@@ -1,11 +1,70 @@
 export default [
     `
-    # Level 0 \n
-    ## Heading 2
+    ## Bot ID #654678 IE. Name: 'Qwerty' \n
+    Created by Central Command: Exploration and Resource Exploitation \n
+    **Equipped with: ** \n
+    1. Automatic Satellite Visual = Online; set coordinates to reestablish and maintain lock \n
+    2. Laser Mining Drill = Offline \n
+    3. Matter Scrambling Assembler = Offline \n
+    **Central Command: ** \n
+    Welcome to Central Command, this is the Command List, or CL where all programming information can be found  \n
+    **ERROR:** Missing memory files, manual initialization is required through terminal \n
+    Loading intitialization program... \n
+    \n
     `,
     `
-    # Level 1.1 \n
-    ## Heading 2
+    **# Basics 1** \n
+    **WARNING:** All standardized bots are required to communicate through the following methods, use of any code outside of convention will lead to deactivation \n
+    **##Operators** \n
+    Operators are symbols that can interact with variables \n
+    = \n
+    x=5 \n
+    **Def:** = used to assign a variable a value, the value of x is now 5 \n
+    == \n
+    x == 5 \n
+    **Def:** == used to compare the value of two variables returning true or false, x does equal 5 and therefore this is true \n
+    \n
+    Values using these operators are controlled, maintained and updated by the Central Command, tampering with such values will lead to deactivation \n
+    \n
+    + \n
+    x = 5+5 \n
+    **Def:** + used to add two numbers \n
+    \n
+    - \n
+    x = 10-5 \n
+    **Def:** - used to subtract two numbers \n
+    \n
+    * \n
+    x = 5*5 \n
+    **Def:** * used to multiply two numbers together \n
+    \n
+    / \n
+    x = 10/5 \n
+    **Def:** / used to divide two numbers \n
+    \n
+    **##Variables:** \n
+    Variables are names assigned to different types of data \n
+    robotName = “Qwerty” \n
+    **Def:** variable above is type **string** with the identity of the text inside quoutes \n
+    🔏 robotHealth = 1 \n
+    🔏 robotBatteryLife = 100 \n
+    **Def:** variables above are type **integer** which take on a whole number value\n
+    **WARNING:** Bot: Qwerty is currently at critical health \n
+    satVideoX = 5.395673954 \n
+    satVideoY = 12.486509372 \n
+    robotX = 5.000000000 \n
+    robotY = 12.000000000 \n
+    **Def:** variables above are type **double** which take on numbers with decimal places \n
+    \n
+    array = [entry1,entry2, …] \n
+    satVideoCordinates = [satVideoX, satVideoY] \n
+    robotCordinates = [robotX, robotY] \n
+    **Def:** An **array**, an organizational tool for saving multiple, often related, data points \n
+    To alter a list either redefine the variables in the list then redefine the list again or input listName.varibleWithinListName[newValue] to change the value of an individual point within a list \n
+    **##Functions**\n
+    Functions are how bots communicate with Central Command to perform tasks\n
+    print(“printed material”)\n
+	**Def:** function print() prints the information within the paranthesis\n
     `,
     `
     # Level 1.2 \n
