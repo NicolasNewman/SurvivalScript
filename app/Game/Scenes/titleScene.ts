dba45986acd483d03ebe2ca2953694ac8dacfceb
Justin Qualley <justin@pal-nat186-157-207.itap.purdue.edu>
2019-09-15 11:57:13 -0400

    added intro in output

--- app/Game/Scenes/titleScene.ts
+++ app/Game/Scenes/titleScene.ts
@@ -22,7 +22,11 @@ export default class TitleScene extends Scene {
     create() {
         this.add.image(415, 330, 'Background').setDisplaySize(830, 720);
         console.log('end create');
-        logOutputWrapper('The Devs say Hi');
+        logOutputWrapper('Survival Script Version 0.1');
+        logOutputWrapper('Game Currently Under Development');
+        logOutputWrapper(
+            "Explore a desolate landscape as Qwerty and learn programming and how to read an API while rebuilding Qwerty's code"
+        );
     }
 
     update(time, delta) {
