7819f3de0e2a0ab8e2d1e5f382f448cbec2a9fe2
NicolasNewman <nanewman01@gmail.com>
2019-09-15 01:34:41 -0400

    fixed extra ">" in output

--- app/reducers/output.ts
+++ app/reducers/output.ts
@@ -14,10 +14,10 @@ export default function output(
 ) {
     switch (action.type) {
         case OutputTypeKeys.LOG_OUTPUT:
+            state.allText[state.allText.length - 1] === '> '
+                ? state.allText.pop()
+                : null;
             const formatText = '> ' + action.text;
-            // const arr = state.allText[state.allText.length - 1].includes('>')
-            //     ? state.allText.pop()
-            //     : state.allText;
             return {
                 allText: [...state.allText, formatText, '> ']
             };
