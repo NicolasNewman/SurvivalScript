ac3764fd8cf3d14dee84c5e04e65d74423dc1dfe
avanishsubbiah <avanishsubbiah1@gmail.com>
2019-09-15 00:07:49 -0400

    Cleaned Code

--- app/Game/Scenes/testScene.ts
+++ app/Game/Scenes/testScene.ts
@@ -7,6 +7,7 @@ import { deflate } from 'zlib';
 // C:\JSProj\SurvivalScript\app\Game\Scenes\assets\tilesets\MarsTileSet.png
 
 export default class ExampleScene extends Scene {
+    //Global Variables
     controls = null;
     initialPlayerX = 1568;
     initialPlayerY = 1568;
@@ -14,6 +15,7 @@ export default class ExampleScene extends Scene {
     playerTileX = 25;
     playerTileY = 25;
 
+    //Preloading Tileset and Character Image
     preload() {
         this.load.image(
             'tiles',
@@ -29,156 +31,135 @@ export default class ExampleScene extends Scene {
             'Game/Scenes/assets/character/TestCharacter.png'
         );
     }
+
+    //Create Function for Game Code
     create() {
-        const map = this.make.tilemap({ key: 'map' });
-
-        const tileset = map.addTilesetImage('MarsTileSet64', 'tiles');
-
+        //Creating map and Tile Set
+        const map = this.make.tilemap({ key: 'map' }); //Creating map
+        const tileset = map.addTilesetImage('MarsTileSet64', 'tiles'); //Adding Tileset to Map
         const belowLayer = map.createStaticLayer('Below Player', tileset, 0, 0);
         const worldLayer = map.createStaticLayer('World', tileset, 0, 0);
         const aboveLayer = map.createStaticLayer('Above Player', tileset, 0, 0);
         const camera = this.cameras.main;
 
-        worldLayer.setCollisionByProperty({ collides: true });
-
+        //Creating Player
         const player = this.add.image(
             this.initialPlayerX,
             this.initialPlayerY,
             'TestCharacter'
         );
 
-        const cursors = this.input.keyboard.createCursorKeys();
-        /*this.controls = new Phaser.Cameras.Controls.FixedKeyControl({
-            camera: camera,
-            left: cursors.left,
-            right: cursors.right,
-            up: cursors.up,
-            down: cursors.down,
-            speed: 0.5
-        }); */
-
+        //Camera Settings
         camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
         camera.startFollow(player);
-        console.log('end create');
 
         //Movement
+
+        //Up
         let wKey = this.input.keyboard.addKey('W');
 
         wKey.on('down', event => {
-            console.log('W pressed');
-            console.log(worldLayer);
+            //Checking Tile Below
             const tileAbove = worldLayer.getTileAtWorldXY(
                 player.x,
                 player.y - (64 + 32),
                 true
             );
-            console.log(tileAbove);
+
+            //Determining if Path is Blocked
             let isBlocked;
             if (tileAbove === null) {
                 isBlocked = false;
-                console.log('tileAbove is null');
             } else if (tileAbove.index === 2 || tileAbove.index === 4) {
                 isBlocked = true;
-                console.log(tileAbove.index);
             }
 
+            //Update Player Position
             if (isBlocked === true) {
-                console.log('tileAbove is Solid');
             } else {
                 let initialY = player.y;
                 player.y = initialY - 64;
-                this.playerTileY -= 1;
-                console.log('player moves');
             }
         });
 
+        //Down
         let dKey = this.input.keyboard.addKey('D');
 
         dKey.on('down', event => {
-            console.log('D pressed');
-            console.log(worldLayer);
+            //Checking Tile Below
             const tileRight = worldLayer.getTileAtWorldXY(
                 player.x + 64,
                 player.y,
                 true
             );
-            console.log(tileRight);
+
+            //Determining if Path is Blocked
             let isBlocked;
             if (tileRight === null) {
                 isBlocked = false;
-                console.log('tileRight is null');
             } else if (tileRight.index === 2 || tileRight.index === 4) {
                 isBlocked = true;
-                console.log(tileRight.index);
             }
 
+            //Update Player Position
             if (isBlocked === true) {
-                console.log('tileRight is Solid');
             } else {
                 let initialX = player.x;
                 player.x = initialX + 64;
-                this.playerTileX += 1;
-                console.log('player moves');
             }
         });
 
+        //Left
         let aKey = this.input.keyboard.addKey('A');
 
         aKey.on('down', event => {
-            console.log('A pressed');
-            console.log(worldLayer);
+            //Checking Tile Below
             const tileLeft = worldLayer.getTileAtWorldXY(
                 player.x - (64 + 32),
                 player.y,
                 true
             );
-            console.log(tileLeft);
+
+            //Determining if Path is Blocked
             let isBlocked;
             if (tileLeft === null) {
                 isBlocked = false;
-                console.log('tileLeft is null');
             } else if (tileLeft.index === 2 || tileLeft.index === 4) {
                 isBlocked = true;
-                console.log(tileLeft.index);
             }
 
+            //Update Player Position
             if (isBlocked === true) {
-                console.log('tileLeft is Solid');
             } else {
                 let initialX = player.x;
                 player.x = initialX - 64;
-                this.playerTileX += 1;
-                console.log('player moves');
             }
         });
 
+        //Right
         let sKey = this.input.keyboard.addKey('S');
 
         sKey.on('down', event => {
-            console.log('S pressed');
-            console.log(worldLayer);
+            //Checking Tile Below
             const tileBelow = worldLayer.getTileAtWorldXY(
                 player.x,
                 player.y + 64,
                 true
             );
-            console.log(tileBelow);
+
+            //Determining if Path is Blocked
             let isBlocked;
             if (tileBelow === null) {
                 isBlocked = false;
-                console.log('tileBelow is null');
             } else if (tileBelow.index === 2 || tileBelow.index === 4) {
                 isBlocked = true;
-                console.log(tileBelow.index);
             }
 
+            //Update Player Position
             if (isBlocked === true) {
-                console.log('tileBelow is Solid');
             } else {
                 let initialY = player.y;
                 player.y = initialY + 64;
-                this.playerTileX += 1;
-                console.log('player moves');
             }
         });
     }
