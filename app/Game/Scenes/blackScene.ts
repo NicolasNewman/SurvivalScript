509a65241692fee6c99c3859dbeb730892c8774c
Justin Qualley <justin@pal-nat186-157-207.itap.purdue.edu>
2019-09-15 11:04:19 -0400

    added gameplay from black screen to world

--- app/Game/Scenes/blackScene.ts
+++ app/Game/Scenes/blackScene.ts
@@ -18,11 +18,13 @@ export default class BlackScene extends Scene {
         );
     }
     userInput = '';
+    userInput2 = '';
     create() {
         const runButton = this.add.image(710, 50, 'runButton');
         runButton.setInteractive();
         runButton.on('pointerdown', () => {
             this.userInput = getCodeWrapper();
+            this.userInput2 = getCodeWrapper();
         });
         clearCodeWrapper();
         clearOutputWrapper();
@@ -44,17 +46,24 @@ export default class BlackScene extends Scene {
     }
 
     update(time, delta) {
-        const keyObj = this.input.keyboard.addKey('Y');
-        if (keyObj.isDown) {
-            this.scene.start('MainScene');
-        }
+        //const keyObj = this.input.keyboard.addKey('Y');
+        // if (keyObj.isDown) {
+        //    this.scene.start('MainScene');
+        //}
         if (this.userInput === 'init') {
-            logOutputWrapper('Initialized');
+            logOutputWrapper('ERROR File Not Found');
             this.userInput = '';
             clearCodeWrapper();
+            logOutputWrapper('ERROR File Not Found');
+            logOutputWrapper('ERROR Corrupted File');
+            logOutputWrapper('ERROR Restore Automatic Satellite Visual');
             logOutputWrapper(
-                'Open Command List tab and read documentation to reorient satellite image'
+                'Find and set satVideoCoordinates equal to robotCoordinates'
             );
+        }
+        if (this.userInput2 === 'satVideoCoordinates == robotCoordinates') {
+            logOutputWrapper('Automatic Satellite Visual Restored');
+            this.scene.start('MainScene');
         }
     }
 }
