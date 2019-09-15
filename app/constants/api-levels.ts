13ca667e996fd88efa3d6723b641849934dc86bd
maxlow11 <low11@purdue.edu>
2019-09-15 01:29:20 -0400

    updated levels api

--- app/constants/api-levels.ts
+++ app/constants/api-levels.ts
@@ -1,79 +1,74 @@
 export const LevelList: Array<string> = [
     `
-    # Level 0 \n
+    # Level 0 
     ## Heading 2
     `,
     `# Level 1.1
     \n## Hello
     \nThis sucks`,
     `
-    # Level 1.2 \n
-    ## Heading 2
-    Control Structures:\n
-    ## Heading 3
-    If(): \n
-    X=5\n
-    If(X == 5) { \n
-    Print(“True”)\n
-    }\n
-    Else{\n
-    Print(”False”)\n
-    }\n
-	Def. An If statement is a conditional and will run or not run a certain code contained within itself depending on the validity of the condition\n
+    # Level 1.2 
+    \n ##Control Structures:
+    \nIf(): 
+    \nX=5
+    \nIf(X == 5) { 
+    \nPrint(“True”)
+    \n}
+    \nElse{
+    \nPrint(”False”)
+    \n}
+	\nDef. An If statement is a conditional and will run or not run a certain code contained within itself depending on the validity of the condition
     \n
-    For():\n
-    For(i = 0; i<5; i++){\n
-    Print(“All Hail Central Command”)\n
-    } \n
-	Def. A For loop completes an action for as long as a statement is true; the example above will print All Hail Central Command 5 times, when i is 0, 1, 2, 3, and 4, but not when i=5\n
-	Note: Varibles can be updated citing themselves in addition to a modifier, this statement(i++) will increases i by one each time the loop is run\n
+    \nFor():
+    \nFor(i = 0; i<5; i++){
+    \nPrint(“All Hail Central Command”)
+    \n} 
+	\nDef. A For loop completes an action for as long as a statement is true; the example above will print All Hail Central Command 5 times, when i is 0, 1, 2, 3, and 4, but not when i=5
+	\nNote: Varibles can be updated citing themselves in addition to a modifier, this statement(i++) will increases i by one each time the loop is run
     \n
-    While():\n
-    If (fileName == corrupted){\n
-    While(fileName=corrupted) {\n
-    clean(fileName)\n
-    }\n
-    }\n
-    Def. A While loop contains a repetitive action that is continued until the criteria is no longer met\n
-    WARNING: The Central Command does not recommend cleaning files by the above method, results may vary\n
+    \nWhile():
+    \nIf (fileName == corrupted){
+    \nWhile(fileName=corrupted) {
+    \nclean(fileName)
+    \n}
+    \n}
+    \nDef. A While loop contains a repetitive action that is continued until the criteria is no longer met
+    \nWARNING: The Central Command does not recommend cleaning files by the above method, results may vary
     `,
     `
-    # Level 1.3 \n
-    ## Heading 2\n
-    Central Command Values Efficiency above all else the following has been shown to increase a bots movement speed drastically\n
-    Control Structures:\n
-    ## Heading 3
-    Functions\n
-    function moveLeft(){\n
-    if(leftButtonDown == True){\n
-    robotX=robotX+ 1\n
-    }\n
-    } \n
-    As a diagnostic of your gathered intelligence recode your drive train for all four cardinal directions and begin moving \n
+    \n# Level 1.3 
+    \n## Heading 2
+    \nCentral Command Values Efficiency above all else the following has been shown to increase a bots movement speed drastically
+    \n##Control Structures:
+    \nFunctions
+    \nfunction moveLeft(){
+    \nif(leftButtonDown == True){
+    \nrobotX=robotX+ 1
+    \n}
+    \n} 
+    \nAs a diagnostic of your gathered intelligence recode your drive train for all four cardinal directions and begin moving 
 
     `,
     `
-    # Level 2 \n
-    ## Heading 2
-    **Code Green:**\n
-    Please Upload(security clearance) to access\n
+    \n# Level 2 
+
+    \n**Code Green:**
+    \nPlease Upload(security clearance) to access
     `,
     `
-    # Level 3 \n
-    ## Heading 2
-    **Code Yellow:**\n
-    Please Upload(security clearance) to access\n    
+    \n# Level 3 
+
+    \n**Code Yellow:**
+    \nPlease Upload(security clearance) to access 
     `,
     `
-    # Level 4 \n
-    ## Heading 2
-    **Code Red:**\n
-    Please Upload(security clearance) to access\n
+    \n# Level 4 
+    \n**Code Red:**
+    \nPlease Upload(security clearance) to access
     `,
     `
-    # Level 5 \n
-    ## Heading 2
-    **_Top Secret: Classified_**\n
-    ACCESS DENIED\n
+    \n# Level 5 
+    \n**_Top Secret: Classified_**
+    \nACCESS DENIED
     `
 ];
